2ccb73b9415379873271dcc7ecee5c704726cf1f
Văn Thế Vinh <vtvinh1412@gmail.com>
2024-03-21 18:19:43 +0700

    update code for new .env format

--- config/envConf.js
+++ config/envConf.js
@@ -4,7 +4,6 @@ const NODE_ENV = process.env.NODE_ENV;
 
 const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL;
-const DB_PASSWORD = process.env.DB_PASSWORD;
 const USER_TOKEN_SECRET = process.env.USER_TOKEN_SECRET;
 const USER_REFRESH_TOKEN_SECRET = process.env.USER_REFRESH_TOKEN_SECRET;
 const YTB_API_URL = process.env.YTB_API_URL;
@@ -13,7 +12,6 @@ const YTB_API_KEY = process.env.YTB_API_KEY;
 module.exports = {
   PORT,
   DB_URL,
-  DB_PASSWORD,
   USER_TOKEN_SECRET,
   USER_REFRESH_TOKEN_SECRET,
   YTB_API_KEY,
