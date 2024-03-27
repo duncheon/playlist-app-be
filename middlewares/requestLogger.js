fe34f1b96b587d13ee549f2bc4f275c70264a09a
Văn Thế Vinh <57767145+duncheon@users.noreply.github.com>
2024-03-27 14:02:09 +0700

    fetch song on scroll (#2)

--- middlewares/requestLogger.js
+++ middlewares/requestLogger.js
@@ -2,6 +2,12 @@ module.exports = function (req, res, next) {
   console.log('Method:', req.method);
   console.log('Path:  ', req.path);
   console.log('Body:  ', req.body);
+  if (Object.keys(req.query).length > 0) {
+    console.log('Query ', req.query);
+  }
+  if (Object.keys(req.params).length > 0) {
+    console.log('Query ', req.params);
+  }
   console.log('---');
   next();
 };
